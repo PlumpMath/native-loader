78aa4062d7d70cdfc790caa907e1152968b65a12
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-13 23:56:27 +0800

    Add more info in project.clj
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.coldnew/native-loader "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.coldnew/native-loader "0.1.0-SNAPSHOT"
+  :description " Native library loader for extracting and loading native libraries from jar."
+  :url "https://github.com/coldnew/native-loader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
@@ -8,13 +8,11 @@
   :source-paths ["src/clj"]
 
   :profiles {:uberjar-test
-             {:dependencies [[opencv/opencv "3.0.0"]
-                             [opencv/opencv-native "3.0.0"]]
+             {:dependencies [[org.clojars.coldnew/opencv "3.0.0"]
+                             [org.clojars.coldnew/opencv-native-macosx-x86_64 "3.0.0-SNAPSHOT"]]
               :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
               :source-paths ["test/clj"]
               :resource-paths ["dev-resources"]
+              :uberjar-name "native-loader-test.jar"
               :aot :all}
              })
-
-;; lein with-profiles uberjar-test  uberjar
-;; java -jar target/native-loader-0.1.0-SNAPSHOT-standalone.jar -m test-native-loader
