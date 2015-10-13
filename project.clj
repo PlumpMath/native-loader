6b5b5a47781698f06f3b35b5b6fd90a3a49a86a2
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-13 14:02:29 +0800

    update project.clj
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -13,10 +13,11 @@
   :java-source-paths ["src/java" "test/java"]
   :junit ["test/java"]
 
-  :profiles {:uberjar
+  :profiles {:uberjar-test
              {:dependencies [[opencv/opencv "3.0.0"]
                              [opencv/opencv-native "3.0.0"]]
               :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
-              :source-paths ["test/clj"]
-              :aot :all
-              } })
+              :source-paths ["src/clj" "test/clj"]
+              :resource-paths ["dev-resources"]
+              :aot :all}
+             })
